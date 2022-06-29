8a61e0c628b1de2b035400db98e9d1257fd39861
Tiago César <55418034+cesartiago@users.noreply.github.com>
2022-06-28 23:51:57 -0300

    Add files via upload

---
+++ Exerc 1 Data e Hora.js
@@ -0,0 +1,11 @@
+//Função Mostrar data e hora
+
+/* COMO ISSO JA É UM ARQUIVO JAVASCRIPT, VC NAO PRECISA DE ESCREER AS TAGS AQUI*/
+            
+        
+function Mostrar_DeH(){
+    document.write (Date ())
+}
+   
+           
+
